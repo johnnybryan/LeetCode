9555ee4363768a5fe5188913d3efb4be20292cd1
John Bryan <91139359+johnnybryan@users.noreply.github.com>
2022-01-25 08:11:32 -0600

    Time: 139 ms (41.16%), Space: 41.3 MB (60.94%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 const removeDuplicates = (nums) => {
-  let i = k = 1;
+  let i = 1, k = 1;
   while (i < nums.length) {
     if (nums[k - 1] !== nums[i]) {
       nums[k] = nums[i];
