6df0ef12530d38cdc341cbfde5141d83d4907548
John Bryan <91139359+johnnybryan@users.noreply.github.com>
2022-02-02 22:40:43 -0500

    Time: 1680 ms (6.36%), Space: 19.4 MB (16.98%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -1,11 +1,25 @@
 class Solution:
+    # def findMaxLength(self, nums: List[int]) -> int:
+    #     dict = {0: 0}
+    #     key, maxLen = 0, 0
+    #     for i in range(len(nums)):
+    #         key += nums[i] or -1
+    #         if key not in dict:
+    #             dict[key] = i + 1
+    #         else:
+    #             maxLen = max(maxLen, i + 1 - dict[key])
+    #     return maxLen
     def findMaxLength(self, nums: List[int]) -> int:
-        dict = {0: 0}
+        d = {0: -1}
         key, maxLen = 0, 0
         for i in range(len(nums)):
-            key += nums[i] or -1
-            if key not in dict:
-                dict[key] = i + 1
+            n = nums[i]
+            if n == 0: 
+                key -= 1
             else:
-                maxLen = max(maxLen, i + 1 - dict[key])
+                key += 1
+            if key in d:
+                maxLen = max(maxLen, i - d[key])
+            else:
+                d[key] = i
         return maxLen
