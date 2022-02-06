d1dc1aedac882bc2d8e031fb629c6f45e44d1798
John Bryan <91139359+johnnybryan@users.noreply.github.com>
2022-02-06 01:39:23 -0500

    Update 108-convert-sorted-array-to-binary-search-tree.py

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
@@ -5,21 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-    #     def binarySearch(left: int, right: int) -> [TreeNode]:
-    #         if left == right:
-    #             return TreeNode(nums[left])
-    #         mid = (left + right)//2
-    #         node = TreeNode(nums[mid])
-    #         if mid > left:
-    #             node.left = binarySearch(left, mid-1)
-    #         if mid < right:
-    #             node.right = binarySearch(mid+1, right)
-    #         return node
-    #     return binarySearch(0, len(nums)-1)
-    #################
-	  # recursive dfs #
-	  #################
+#################
+# recursive dfs #1
+#################
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def binarySearch(left: int, right: int) -> [TreeNode]:
+            if left == right:
+                return TreeNode(nums[left])
+            mid = (left + right)//2
+            node = TreeNode(nums[mid])
+            if mid > left:
+                node.left = binarySearch(left, mid-1)
+            if mid < right:
+                node.right = binarySearch(mid+1, right)
+            return node
+        return binarySearch(0, len(nums)-1)
+#################
+# recursive dfs #2
+#################
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         if not nums:
             return None
@@ -28,24 +31,24 @@ class Solution:
         root.left = self.sortedArrayToBST(nums[:mid])
         root.right = self.sortedArrayToBST(nums[mid+1:])
         return root
-    #################
-	  # iterative dfs #
-	  #################
-#     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-#         if not nums:
-#             return None
+#################
+# iterative dfs #
+#################
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
 
-#         root = TreeNode()
-#         # (node, lower, upper) with [lower, upper)
-#         stack = [(root, 0, len(nums))]
+        root = TreeNode()
+        # (node, lower, upper) with [lower, upper)
+        stack = [(root, 0, len(nums))]
         
-#         while stack:
-#             node, lower, upper = stack.pop()
-#             mid = (lower + upper) // 2
-#             node.val = nums[mid]
-#             node.left = TreeNode() if mid > lower else None
-#             node.right = TreeNode() if upper > mid + 1 else None
-#             if node.right: stack.append((node.right, mid+1, upper))
-#             if node.left: stack.append((node.left, lower, mid))
+        while stack:
+            node, lower, upper = stack.pop()
+            mid = (lower + upper) // 2
+            node.val = nums[mid]
+            node.left = TreeNode() if mid > lower else None
+            node.right = TreeNode() if upper > mid + 1 else None
+            if node.right: stack.append((node.right, mid+1, upper))
+            if node.left: stack.append((node.left, lower, mid))
                 
-#         return root
+        return root
