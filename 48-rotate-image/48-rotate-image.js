711ece8f87c9d16889638abdaabc874f4c061020
John Bryan <91139359+johnnybryan@users.noreply.github.com>
2022-02-12 19:06:15 -0500

    Time: 48 ms (99.96%), Space: 42.4 MB (16.06%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -17,19 +17,11 @@ const rotate = (matrix) => {
       matrix [j][i] = temp;
     }
   }
-		
   //[1,4,7]   >>>>    [7,4,1]
   //[2,5,8]   >>>>    [8,5,2]
   //[3,6,9]   >>>>    [9,6,3]
   //swap first and last el from each row in this case; we swap 1 and 7 and it becomes 7 and 1
-  // for(let i = 0; i < n; i++){
-  //    for(let j = 0; j < (n/2); j++){
-  //       let temp = matrix[i][j];
-  //       matrix[i][j] = matrix[i][n-1-j];
-  //       matrix[i][n-1-j] = temp;
-  //     }
-  // }
-  for(let i = 0; i< n; i++){
+  for(let i = 0; i < n; i++){
     matrix[i].reverse() 
   } 
 };
