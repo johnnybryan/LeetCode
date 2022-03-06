be714203081bcbe113d6a5c747a14c0358504851
John Bryan <91139359+johnnybryan@users.noreply.github.com>
2022-03-05 19:27:22 -0500

    Time: 421 ms (54.97%), Space: 16.4 MB (76.19%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,21 @@
+class Solution:
+  
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        num_isles = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    num_isles += 1
+                    self.dfs(grid, r, c)
+        return num_isles
+    
+    def dfs(self, grid, r, c):
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != '1': 
+            return # if land is visited or water or if out of bounds: exit function
+        grid[r][c] = '#' # mark island as visited, explore surrounded land
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r, c + 1)
+        self.dfs(grid, r, c - 1)
